45c6ad14178e183a55b33429c4a632fcbad01d4e
fadhlu <fadhlurahman.i@gmail.com>
2022-03-10 13:21:51 +0800

    daisyui init

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import Hero from '../components/Hero';
+
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen bg-blue-400">
-      <h1 className="text-8xl text-white">Hello Next.js</h1>
+      <Hero />
     </div>
   );
 }
