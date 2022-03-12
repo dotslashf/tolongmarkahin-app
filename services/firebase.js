a6f2129df154882f179341594ab4cc2cda7db8bc
fadhlu <fadhlurahman.i@gmail.com>
2022-03-12 11:48:52 +0800

    add key

--- services/firebase.js
+++ services/firebase.js
@@ -3,7 +3,11 @@ import serviceAccountKey from '../serviceAccountKey.json';
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountKey),
+    credential: admin.credential.cert(
+      process.env.FIREBASE_SERVICE_ACCOUNT_KEY
+        ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
+        : serviceAccountKey
+    ),
   });
 }
 
