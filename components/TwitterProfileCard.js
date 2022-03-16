1e075ae2e8eb207bb6d616260481ee76012d8b87
fadhlu <fadhlurahman.i@gmail.com>
2022-03-16 23:10:54 +0800

    fix ui

--- components/TwitterProfileCard.js
+++ components/TwitterProfileCard.js
@@ -11,7 +11,7 @@ export default function TwitterProfileCard() {
   if (!data) return <TwitterProfileCardLoading />;
 
   return (
-    <div className="card card-compact bg-base-100">
+    <div className="card card-compact bg-base-100 shadow-md">
       <figure>
         <img
           src={
@@ -25,27 +25,38 @@ export default function TwitterProfileCard() {
       </figure>
       <div className="card-body">
         <h2 className="card-title">
+          <div className="avatar">
+            <div className="w-16 rounded-box">
+              <img
+                src={`${data.profile_image_url.replace('_normal', '')}`}
+                alt={`${data.screen_name}`}
+              />
+            </div>
+          </div>
           {data.name} @{data.screen_name}
         </h2>
         <p>{data.description}</p>
-        <div></div>
-        <div className="card-actions justify-start">
-          <div className="flex space-x-2">
-            <span className="badge badge-primary text-base-100">
-              following: {data.friends_count}
-            </span>
-            <span className="badge badge-primary text-base-100">
-              followers: {data.followers_count}
-            </span>
-          </div>
-        </div>
         <div className="card-actions justify-start mt-4">
           <button
-            className="btn btn-sm btn-accent"
+            className="btn btn-sm btn-accent gap-2"
             onClick={() => {
               window.open(`https://twitter.com/${data.screen_name}`);
             }}
           >
+            <svg
+              className="w-4 h-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
             Ke Profile
           </button>
           <button
@@ -73,6 +84,48 @@ export default function TwitterProfileCard() {
           </button>
         </div>
       </div>
+      <div className="stats stats-vertical shadow-md">
+        <div className="stat">
+          <div className="stat-figure text-primary">
+            <svg
+              className="w-8"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
+              ></path>
+            </svg>
+          </div>
+          <div className="stat-title">Followers</div>
+          <div className="stat-value text-primary">{data.followers_count}</div>
+        </div>
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <svg
+              className="w-8"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+              ></path>
+            </svg>
+          </div>
+          <div className="stat-title">Following</div>
+          <div className="stat-value text-secondary">{data.friends_count}</div>
+        </div>
+      </div>
     </div>
   );
 }
@@ -86,14 +139,51 @@ function TwitterProfileCardLoading() {
       <div className="card-body p-4">
         <div className="bg-gray-400 h-8 rounded-md"></div>
         <div className="bg-gray-400 h-6 rounded-md"></div>
-        <div className="card-actions justify-start">
-          <div className="flex space-x-2">
-            <div className="rounded-full h-4 bg-gray-400 w-24"></div>
-            <div className="rounded-full h-4 bg-gray-400 w-24"></div>
-          </div>
+        <div className="card-actions mt-4">
+          <button className="rounded-md h-6 bg-gray-400 w-24"></button>
+          <button className="rounded-md h-6 bg-gray-400 w-24"></button>
         </div>
-        <div className="card-actions justify-end mt-4">
-          <button className="rounded-md h-8 bg-gray-400 w-24"></button>
+      </div>
+      <div className="stats stats-vertical shadow-md">
+        <div className="stat">
+          <div className="stat-figure text-gray-400">
+            <svg
+              className="w-8"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
+              ></path>
+            </svg>
+          </div>
+          <div className="stat-title">Followers</div>
+          <div className="stat-value text-gray-400">000</div>
+        </div>
+        <div className="stat">
+          <div className="stat-figure text-gray-400">
+            <svg
+              className="w-8"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+              ></path>
+            </svg>
+          </div>
+          <div className="stat-title">Following</div>
+          <div className="stat-value text-gray-400">000</div>
         </div>
       </div>
     </div>
